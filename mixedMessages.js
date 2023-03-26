06048284b0a9626c775b758b1742600f2a053ac8
Keith <69634519+cheeseStack@users.noreply.github.com>
2023-03-26 14:31:36 +0100

    called chooseOption function for the bodyPart, equipment, and achieve options

--- mixedMessages.js
+++ mixedMessages.js
@@ -16,27 +16,39 @@
 */
 
 // 1. Body parts to work out / exercise
-const bodyParts = ['fingers', 'thumbs', 'forearm', 'biceps', 'shoulders', 'back', 'chest', 'abdominals', 'hips', 'quadriceps', 'calves', 'anlles', 'toes'];
+const bodyPartOptions = ['fingers', 'thumbs', 'forearm', 'biceps', 'shoulders', 'back', 'chest', 'abdominals', 'hips', 'quadriceps', 'calves', 'ankles', 'toes'];
 
 
 // 2. exercise equipment
-const equipment = ['barbells', 'dumbells', 'TRX machine', 'treadmill', 'rowing machine', 'exercise bike', 'resistance bands', 'cans of beans' ]
+const equipmentOptions = ['barbells', 'dumbells', 'TRX machine', 'treadmill', 'rowing machine', 'exercise bike', 'resistance bands', 'cans of beans' ]
 
 
 // 3. Exercise duration between 1 and 10 minutes
-const duration = Math.ceil(Math.random() * 10)
+// This does not need its own function or further action
+const durationOptions = Math.ceil(Math.random() * 10)
 
 
 // 4. What will the exercises achieve
-const achieve = ['eternal happiness', 'eternal life', 'respect from the world', 'immense wealth', 'elite athlete status', 'a world record', '3 wishes', 'a mystery prize', "a lifetime's supply of cheese", 'everything your heart desires']
+const achieveOptions = ['eternal happiness', 'eternal life', 'respect from the world', 'immense wealth', 'elite athlete status', 'a world record', '3 wishes', 'a mystery prize', "a lifetime's supply of cheese", 'everything your heart desires']
 
 
-//  Create a random index functions based on the array length
+// a) Create a random index functions based on the array length
 const chooseRandomIndex = arr =>  Math.floor(Math.random() * arr.length)
 
 
-// 1. Choose body part
-const chooseBodyPart = () =>  bodyParts[chooseRandomIndex(bodyParts)]
+// b) Function to select an option from any array
+const chooseOption = arr =>  arr[chooseRandomIndex(arr)]
 
 
-console.log(chooseBodyPart())
+// c) generate choices
+
+    // i) body part
+const bodyPart = chooseOption(bodyPartOptions)
+
+    // ii) equipment
+const equipment = chooseOption(equipmentOptions)
+
+    // iii) achieve options 
+const achieve = chooseOption(achieveOptions)
+
+// d) user message:
